46a252fd6223e71e3938669450652b8f1c6c4f09
日野義斗 <hino.akito@nck.co.jp>
2024-01-25 22:59:56 +0900

    feat: マテリアルアイコンの拡張

--- app/src/main/java/com/example/sakeapp/ui/component/BottomAppBar.kt
+++ app/src/main/java/com/example/sakeapp/ui/component/BottomAppBar.kt
@@ -1,14 +1,51 @@
 package com.example.sakeapp.ui.component
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.StarRate
+import androidx.compose.material.icons.filled.Timelapse
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.sakeapp.ui.theme.BlueGrey
+import com.example.sakeapp.ui.theme.Grey20
 
 @Composable
 fun SakeBottomAppBar(){
-        BottomAppBar(
-            containerColor = BlueGrey,
-        ) {
+        BottomAppBar(containerColor = Grey20) {
+            Row {
+                IconButton(
+                    onClick = {/*TODO クリックイベント*/}
+                ) {
+                    Icon(
+                        Icons.Filled.Timelapse,
+                        contentDescription = "タイムライン",
+                        tint = BlueGrey,
+                        modifier = Modifier.size(100.dp)
+                    )
+                }
 
+                Spacer(modifier = Modifier.width(100.dp))
+
+                IconButton(
+                    onClick = {/*TODO クリックイベント*/}
+                ) {
+                    Icon(
+                        Icons.Filled.Create,
+                        contentDescription = "投稿",
+                        tint = BlueGrey,
+                        modifier = Modifier.size(100.dp)
+                    )
+                }
+            }
         }
 }
--- app/src/main/java/com/example/sakeapp/ui/component/TopAppBar.kt
+++ app/src/main/java/com/example/sakeapp/ui/component/TopAppBar.kt
@@ -1,12 +1,8 @@
 package com.example.sakeapp.ui.component
 
-import androidx.compose.foundation.layout.Row
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material3.Badge
-import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -14,9 +10,8 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import com.example.sakeapp.ui.theme.BlueGrey
-import kotlinx.coroutines.launch
+import com.example.sakeapp.ui.theme.Grey20
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -30,14 +25,14 @@ fun SakeTopAppBar() {
                 Icon(
                     Icons.Filled.Menu,
                     contentDescription = "ハンバーガーメニュー",
-                    tint = Color.Gray
+                    tint = Grey20
                 )
             }
         },
         title = {
                 Text(
                     text = "さけのば",
-                    color = Color.White
+                    color = Grey20
                 )
         },
         actions = {
@@ -45,7 +40,7 @@ fun SakeTopAppBar() {
                  Icon(
                      Icons.Filled.Info,
                      contentDescription = "CopyRight",
-                     tint = Color.Gray
+                     tint = Grey20
                  )
             }
         }
