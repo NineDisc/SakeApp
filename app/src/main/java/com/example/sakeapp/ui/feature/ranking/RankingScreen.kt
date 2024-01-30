fb8880b756dfc3739639fddf016a25349d98d5b0
NineDisc <queue4423@gmail.com>
2024-01-30 21:28:45 +0900

    feat:ランキング画面のUI調整

--- app/src/main/java/com/example/sakeapp/ui/feature/ranking/RankingScreen.kt
+++ app/src/main/java/com/example/sakeapp/ui/feature/ranking/RankingScreen.kt
@@ -3,19 +3,19 @@ package com.example.sakeapp.ui.feature.ranking
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.sakeapp.model.SakeRanking
 import com.example.sakeapp.ui.theme.SakeAppTheme
 
@@ -40,6 +40,7 @@ fun RankingScreen() {
         ) {
             items(items = viewModel.rankingList) { item ->
                 RankingItem(item = item)
+                Divider(thickness = 1.dp, color = Color.Black)
             }
         }
     }
@@ -47,17 +48,27 @@ fun RankingScreen() {
 
 @Composable
 fun RankingItem(item: SakeRanking) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
+
+
+    Column(
         modifier = Modifier
             .fillMaxWidth()
-            .height(200.dp)
-            .background(
-                Color.Yellow
-            )
+//            .height(400.dp)
+            .background(Color.White)
+            .padding(16.dp)
     ) {
-        Text(text = "${item.rank}: ${item.name}")
+        Text(
+            text = "${item.rank}位",
+            fontSize = 48.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Text(text = "フリガナ")
+        Text(text = item.name, fontSize = 36.sp)
+        Text(text = "${item.breweries}  ${item.area}")
     }
+
 }
 
 @Preview
@@ -66,5 +77,18 @@ fun RankingScreenPreview() {
     SakeAppTheme {
 //        RankingScreen()
     }
+}
+
+@Preview
+@Composable
+fun RankingItemPreview() {
+    val item = SakeRanking(
+        rank = 1,
+        name = "田酒",
+        breweries = "新政酒造",
+        area = "秋田県"
+    )
+
+    RankingItem(item = item)
 }
 
