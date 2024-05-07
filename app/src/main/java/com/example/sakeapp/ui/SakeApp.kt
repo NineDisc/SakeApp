2b76608dade8d45ec013c6a6040fdf1751e92f4d
NineDisc <queue4423@gmail.com>
2024-05-07 20:33:48 +0900

    refactor:アニメーションリファクタ

--- app/src/main/java/com/example/sakeapp/ui/SakeApp.kt
+++ app/src/main/java/com/example/sakeapp/ui/SakeApp.kt
@@ -1,5 +1,7 @@
 package com.example.sakeapp.ui
 
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
@@ -7,12 +9,15 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
 import androidx.compose.animation.shrinkOut
+import androidx.compose.animation.slideIn
+import androidx.compose.animation.slideOut
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -24,6 +29,13 @@ import com.example.sakeapp.ui.feature.ranking.RankingContents
 @Composable
 fun SakeApp() {
     val navController = rememberNavController()
+    val enterAnimation = slideIn(tween(100, easing = LinearOutSlowInEasing)) {
+        IntOffset(it.width / 4, 100)
+    }
+    val exitAnimation = slideOut(tween(100, easing = FastOutSlowInEasing)) {
+        IntOffset(-180, 50)
+    }
+
 
     Scaffold(
         topBar = { SakeTopAppBar(navController) },
@@ -48,8 +60,12 @@ fun SakeApp() {
                         ), targetScale = 2f
                     ) + fadeOut(tween(delayMillis = 90))
                 },
-                popEnterTransition = { expandVertically { 200 } },
-                popExitTransition = { shrinkOut { IntSize(200, 300) } }) {
+                popEnterTransition = {
+                    expandVertically { 200 }
+                },
+                popExitTransition = {
+                    shrinkOut { IntSize(200, 300) }
+                }) {
                 composable("ranking") { RankingContents() }
                 composable("starScreen") { StarIconScreen() }
                 composable("search") { SearchScreen() }
